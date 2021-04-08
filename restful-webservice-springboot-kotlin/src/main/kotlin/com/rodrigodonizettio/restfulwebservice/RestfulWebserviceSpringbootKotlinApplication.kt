2951917a87b71b5fb58f60c32ac5aed4381ef30a
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2021-04-08 20:20:14 -0300

    Starting a Spring Boot project from Spring Initializr

---
+++ restful-webservice-springboot-kotlin/src/main/kotlin/com/rodrigodonizettio/restfulwebservice/RestfulWebserviceSpringbootKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.rodrigodonizettio.restfulwebservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestfulWebserviceSpringbootKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestfulWebserviceSpringbootKotlinApplication>(*args)
+}
